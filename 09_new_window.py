5af05b7ea1fc50fdd8dfc7f75752047db5369145
maplexuci <zhhxu9@gmail.com>
2021-01-17 01:04:35 -0500

    Create 09_new_window.py

---
+++ 09_new_window.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title("Create New Window")
+root.iconbitmap('images/icon.ico')
+
+
+def openNew():
+    global my_img
+    # We need to make this variable as global, 
+    # you could understand it like this: the button which calls the function is in the root window,
+    # my_img is a local variable, when you need to load image in the second window,
+    # python thinks this local variable as garbage, and go through its garbage collection mechanism.
+    # Therefore, making it as global letting the new window is able to access this variable. 
+    # However, local variable works in the root window.
+    top = Toplevel()
+    top.title("Second Window")
+    lbl = Label(top, text="This is the second window").pack()
+    my_img = ImageTk.PhotoImage(Image.open("images/1.jpg"))
+    img_lbl= Label(top, image=my_img).pack()
+    btn2 = Button(top, text="close window", command=top.destroy).pack()
+
+
+button = Button(root, text="Oen the second window", command=openNew).pack()
+
+root.mainloop()
